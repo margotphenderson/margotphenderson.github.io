0ef5356778550616ca8d6cb8625915f5e75799fc
Margot <margoth@andrew.cmu.edu>
2019-12-06 20:23:56 -0500

    testing rotating links

--- assgn8/Assets/script.js
+++ assgn8/Assets/script.js
@@ -4,34 +4,11 @@ window.addEventListener("scroll", function() {
     goldenRecordOrig.style.transform = "rotate("+window.pageYOffset+"deg)";
 });
 
-// ScrollReveal().reveal('.headline');
-// ScrollReveal().reveal('.tagline', { delay: 500 });
-// ScrollReveal().reveal('.punchline', { delay: 2000 });
-
-// console.log(document.getElementById('gr-orig'))
-// console.log(document.getElementById('spacer'))
-//
-// var recordHeight=document.getElementById('gr-orig').style.height;
-// var spacerHeight=document.getElementById('spacer').style.height;
-// console.log("spacer height =" + spacerHeight)
-// console.log("record height =" + recordHeight)
-//
-// document.getElementById('gr-orig').style.height=spacerHeight;
-// console.log("new record height =" + recordHeight)
-
-// if(recordHeight>(spacerHeight*2))
-// {
-//     document.getElementById('gr-orig').style.height=(spacerHeight*2);
-// }
-// else
-// {
-//     document.getElementById('leftdiv').style.height=right;
-// }
-
-
-
-
-
+//rotate links on scroll
+// var allLinks = document.getElementById("all-links");
+// window.addEventListener("scroll", function() {
+//     allLinks.style.transform = "rotate("+window.pageYOffset+"deg)";
+// });
 
 
 // ===== Scroll to Top (rocket) by  https://codepen.io/Askwithloud/pen/yNRPWB
