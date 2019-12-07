0b00abe1870084c54d649401bf7cc8e6d9edb301
Margot <margoth@andrew.cmu.edu>
2019-12-06 22:38:21 -0500

    HW8

--- assgn8/Assets/script.js
+++ assgn8/Assets/script.js
@@ -11,6 +11,27 @@ window.addEventListener("scroll", function() {
 // });
 
 
+// Fade in elements on scroll by  https://codepen.io/obadaa/pen/VQQYpN
+// $(function(){  // $(document).ready shorthand
+//   $('.container').fadeIn('slow');
+// });
+// 
+// $(document).ready(function() {
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+//         /* Check the location of each desired element */
+//         $('.hideme').each( function(i){
+//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+//             /* If the object is completely visible in the window, fade it it */
+//             if( bottom_of_window > bottom_of_object ){
+//                 $(this).animate({'opacity':'1'},1500);
+//             }
+//         });
+//     });
+// });
+
+
 // ===== Scroll to Top (rocket) by  https://codepen.io/Askwithloud/pen/yNRPWB
 
 jQuery(window).scroll(function(){
